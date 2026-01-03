11b62d761ab6436ab47403dc4a1bdec8e1fbb522
Ulysse <ulysse.rousselet@epitech.eu>
2026-01-04 00:51:07 +0100

    add: implement fetch and execute_loop functions for opcode processing

--- src/systems/execute_system.cpp
+++ src/systems/execute_system.cpp
@@ -7,11 +7,33 @@
 
 #include "C_Systems.hpp"
 
+uint8_t fetch(Registry &registry, CPU &cpu, Memory &mem)
+{
+    return mem.data[cpu.PC++];
+}
+
+void execute_loop(Registry &registry, CPU &cpu, Memory &mem)
+{
+    uint8_t opcode = fetch(registry, cpu, mem);
+    printf("PC = %d, valeur: %d, valeur EXA: %02X\n", cpu.PC - 1, opcode, opcode);
+    switch (opcode) {
+        case 0x00:
+            break;
+        case 0xC3:
+            {
+                uint8_t low = fetch(registry, cpu, mem);
+                uint8_t high = fetch(registry, cpu, mem);
+                cpu.PC = (high << 8) | low;
+            }
+        break;
+    }
+}
+
 void execute_system(Registry &registry, Sparse_array<CPU> &cpu, Sparse_array<Memory> &mem)
 {
     for (size_t i = 0; i < cpu.size() && i < mem.size(); i++) {
         if (cpu[i].has_value() && mem[i].has_value()) {
-            std::cout << "test" << "\n";
+            execute_loop(registry, cpu[i].value(), mem[i].value());
         }
     }
 }
