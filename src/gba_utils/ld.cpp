529bd7762da6856537e103dd556809b9728c2003
Ulysse <ulysse.rousselet@epitech.eu>
2026-01-05 18:03:07 +0100

    add: implement additional load functions for CPU operations

--- src/gba_utils/ld.cpp
+++ src/gba_utils/ld.cpp
@@ -1,11 +1,29 @@
 #include "GbaUtils.hpp"
 
+void ld_02(CPU &cpu, Memory &mem)
+{
+    mem.data[(cpu.B << 8) | cpu.C] = cpu.A;
+}
+
+void ld_12(CPU &cpu, Memory &mem)
+{
+    mem.data[(cpu.D << 8) | cpu.E] = cpu.A;
+}
+
 void ld_21(CPU &cpu, Memory &mem)
 {
     uint8_t low = fetch(cpu, mem);
     uint8_t high = fetch(cpu, mem);
     cpu.L = low;
     cpu.H = high;
+}
+
+void ld_22(CPU &cpu, Memory &mem)
+{
+    mem.data[(cpu.H << 8) | cpu.L] = cpu.A;
+    cpu.L++;
+    if (cpu.L == 0)
+        cpu.H++;
 }
 
 void ld_0E(CPU &cpu, Memory &mem)
